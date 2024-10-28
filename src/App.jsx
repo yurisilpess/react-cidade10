8d92bb6f7645021013f6a902096afb3e2fa7e67d
YuriSP <yuri.dinodude08@gmail.com>
2024-10-28 10:43:15 -0300

    sidebar beginning

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom'
+import Side from './components/Nav/Side.jsx'
+
+function App() {
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav/Side.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Side = () => {
+  return (
+    <div>Side</div>
+  )
+}
+
+export default Side
